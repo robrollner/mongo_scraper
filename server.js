1624b1f07885c6fbade500059a0f2b998a716148
Robert Rollner <robrollner@gmail.com>
2017-11-19 19:30:47 -0700

    deleted routes, added app.js file.

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const cheerio = require('cheerio'); //installed
 const request = require('request'); //installed
 
 
-// var db = require('./models');
+const db = require('./models');
 const app = express();
-const PORT = process.env.PORT || 6666;
+const PORT = process.env.PORT || 3000;
 var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
-// const routes = require('./routes')
+
 
 app.use(logger('dev'));
 
@@ -31,6 +31,38 @@ app.use(express.static('public'));
 
 app.engine('handlebars', exphbs({ defaultLayout: "main" }));
 app.set('view-engine', 'handlebars');
+
+//app routes
+app.get("/index", (req, res) => {
+    res.render("index", data);
+})
+
+
+
+app.get("/scrape", (req, res) => {
+    request.get("https://www.nytimes.com/").then((response) => {
+        var $ = cheerio.load(response.data);
+
+        $("article h2").each((i, element) => {
+            let result = {};
+            result.title = $(this).children("a").text();
+            result.link = $(this).children("a").attr("href");
+
+            db.Article.create(result).then((dbArticle) => {
+                console.log(result)
+                res.send("Scrape Complete!");
+            }).catch((err) => {
+                console.log(err);
+                res.json(err);
+            })
+
+        })
+    })
+})
+
+
+
+
 
 app.listen(PORT, () => {
     console.log("It's ALIVE!!! " + PORT + ' !!!!');
